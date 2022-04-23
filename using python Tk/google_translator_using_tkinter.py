5a5c973c9ac125a6b24e03f0cb47db1f9c929202
jayson7 <lexxiijoo70@gmail.com>
2022-04-23 18:38:33 +0100

    using google translat onlyls

---
+++ using python Tk/google_translator_using_tkinter.py
@@ -0,0 +1,17 @@
+from tkinter import *
+# build language translator using tkinter and googletrans
+from googletrans import Translator
+
+root = Tk()
+root.title("Google Translator - Jayson")
+root.geometry("880x400")
+
+
+
+
+
+
+
+
+root.mainloop()
+
