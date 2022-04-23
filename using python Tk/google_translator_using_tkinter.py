058b254788f834d0a0999e49bf0c971161957334
jayson7 <lexxiijoo70@gmail.com>
2022-04-23 21:35:48 +0100

    tkinter & translator

--- using python Tk/google_translator_using_tkinter.py
+++ using python Tk/google_translator_using_tkinter.py
@@ -8,6 +8,8 @@ root.title("Google Translator - Jayson")
 root.geometry("800x400")
 root.maxsize('800','400')
 
+# Text boxes 
+
 def translate_it():
 
     pass
@@ -17,10 +19,10 @@ def language_list():
     pass
 
 def  clear():
+    original_text.delete(1.0, END)
+    translated_text.delete(1.0, END)
     
-    pass
 
-# Text boxes 
 original_text = Text(root, width=30, height=20)
 original_text.grid(row=0, column=0, padx=20, pady=10) 
 
@@ -36,9 +38,9 @@ original_combo = ttk.Combobox(root, width=30, value=language_list)
 original_combo.current(21)
 original_combo.grid(row=1, column=0)
 
-translate_combo = ttk.Combobox(root, width=30, value=language_list)
-original_combo.current(21)
-original_combo.grid(row=1, column=0)
+translated_combo = ttk.Combobox(root, width=30, value=language_list)
+translated_combo.current(26)
+translated_combo.grid(row=2, column=2)
 
 
 # clear button 
